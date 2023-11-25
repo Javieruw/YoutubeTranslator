3bdbb8244b905a562ae0322649d86d1d17cb3e23
Javieruw <javier.uw@gmail.com>
2023-11-25 16:27:55 +0100

    Update FinalServer.py

--- FinalServer.py
+++ FinalServer.py
@@ -3,48 +3,8 @@ import whisper
 from pytube import YouTube
 import moviepy.editor as mp
 import os
-from flask_cors import CORS
-
 
 app = Flask(__name__)
-CORS(app)
-
-# @app.route('/process_string', methods=['POST'])
-# def process_string():
-#      # client_ip = request.remote_addr
-#      # server_ip = '154.62.108.77'  # Replace with your server's IP address
-#     #
-#      # Check if the client IP matches the server IP
-#     # if client_ip != server_ip:
-#     #     return jsonify({"error": "Access denied"}), 403
-#
-#     data = request.json
-#     input_string = data['input']
-#
-#     # Process the string here (this example just echoes it back)
-#     yt = YouTube(input_string)
-#     video = yt.streams.filter(only_audio=True).first()
-#     out_file = video.download()
-#
-#     # Saving the file as MP3
-#     base, ext = os.path.splitext(out_file)
-#     new_file = base + '.mp3'
-#     clip = mp.AudioFileClip(out_file)
-#     clip.write_audiofile(new_file)
-#
-#     # Optionally, remove the original download (MP4 file)
-#     os.remove(out_file)
-#
-#     model = whisper.load_model("base")
-#     print("transcription on the way")
-#     result = model.transcribe(base + ".mp3")
-#     os.remove(new_file)
-#
-#     return jsonify({"output": result["text"]})
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 @app.route('/')
 def index():
