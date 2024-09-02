0b9bc5d0dcbeda9d45e20cceb9053b248150bc56
Javieruw <javier.uw@gmail.com>
2024-09-02 21:48:35 +0200

    Update FinalServer.py

--- FinalServer.py
+++ FinalServer.py
@@ -1,53 +1,75 @@
 from flask import Flask, request, jsonify, render_template
 import whisper
-from pytube import YouTube
+import yt_dlp as ytdlp
 import moviepy.editor as mp
 import os
 from celery import Celery
+from flask import url_for
 
 app = Flask(__name__)
 
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
-    )
-    celery.conf.update(app.config)
-    return celery
+# Celery configuration
+app.config['broker_url'] = 'redis://redis:6379/0'
+app.config['result_backend'] = 'redis://redis:6379/0'
+app.config['worker_concurrency'] = int('1')
+app.config['task_acks_late'] = True
+app.config['worker_prefetch_multiplier'] = int('1')
 
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['broker_url'])
+celery.conf.update(app.config)
 
-celery = make_celery(app)
-
-@celery.task
+@celery.task(time_limit=2000)
 def process_video(input_string):
-    print("Downloading YouTube video")
-    yt = YouTube(input_string)
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download()
+    try:
+        print("Downloading YouTube video")
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': '/app/%(title)s.%(ext)s',
+        }
+        with ytdlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(input_string, download=True)
+            file_name = ydl.prepare_filename(info_dict)
 
-    print("Converting to MP3")
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    clip = mp.AudioFileClip(out_file)
-    clip.write_audiofile(new_file)
+        # Convert the downloaded video to MP3
+        print("Converting to MP3")
+        base, ext = os.path.splitext(file_name)
+        new_file = base + '.mp3'
+        clip = mp.AudioFileClip(file_name)
+        clip.write_audiofile(new_file)
 
-    print("Removing original MP4 file")
-    os.remove(out_file)
+        # Remove the original file
+        if os.path.exists(file_name):
+            print(f"Removing original file: {file_name}")
+            os.remove(file_name)
 
-    print("Loading Whisper model and transcribing")
-    model = whisper.load_model("base")
-    result = model.transcribe(new_file)
+        # Load Whisper model and transcribe the audio
+        print("Loading Whisper model and transcribing")
+        model = whisper.load_model("base")
+        result = model.transcribe(new_file)
 
-    print("Cleaning up MP3 file")
-    os.remove(new_file)
+        # Clean up MP3 file after transcription
+        print("Cleaning up MP3 file")
+        os.remove(new_file)
 
-    return result["text"]
+        words = []
+        for segment in result.get("segments", []):
+            for word_info in segment.get("words", []):
+                words.append({
+                    "word": word_info["word"],
+                    "start": word_info["start"],
+                    "end": word_info["end"]
+                })
+
+        return result["text"]
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return {"error": str(e)}
 
 @app.route('/')
 def index():
+    icon_url = url_for('static', filename='jp_icon.ico')
     return render_template('YoutubeTranslator.html')
 
 @app.route('/process_string', methods=['POST'])
@@ -57,7 +79,7 @@ def process_string():
         data = request.json
         input_string = data['input']
 
-        # Start the Celery task
+        # Start the Celery task if worker is available:
         task = process_video.delay(input_string)
 
         print("Task submitted to Celery")
@@ -67,5 +89,28 @@ def process_string():
         print(f"An error occurred: {e}")
         return jsonify({"error": str(e)}), 500
 
+@app.route('/get_task_result/<task_id>', methods=['GET'])
+def get_task_result(task_id):
+    task = process_video.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # Task is still processing
+        response = {
+            'state': task.state,
+            'status': 'Task is still processing'
+        }
+    elif task.state != 'FAILURE':
+        # Task completed successfully
+        response = {
+            'state': task.state,
+            'result': task.result
+        }
+    else:
+        # Something went wrong in the background job
+        response = {
+            'state': task.state,
+            'status': str(task.info)  # Exception raised
+        }
+    return jsonify(response)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=5060, debug=False, threaded=True)
